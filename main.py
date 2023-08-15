bd086a3a6d985e603bbe05230cb8c5b2acf38c7c
NikiDumb <tentotennot@mail.ru>
2023-08-15 04:28:38 +0300

    new folder

--- main.py
+++ main.py
@@ -1,75 +1,23 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, field_validator
-from sqlalchemy import create_engine, Column, Integer, String, Date
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from loguru import logger
+from typing import Optional
 import re
 import uvicorn
 
 # Создание приложения FastAPI
 app = FastAPI()
 
-# CORS (на ютубе мужик сказал что он очень нужен для деплоя)
-#app.add_middleware(
-#    CORSMiddleware,
-#    allow_origins=["*"],
-#    allow_methods=["GET", "POST", "PUT", "DELETE"],
-#    allow_headers=["*"],
-#)
 
 # Подключение к базе данных PostgreSQL
 DATABASE_URL = "postgresql://employeeapi:restapi@localhost/postgres"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-class Base(DeclarativeBase):
-    pass
-
-# Модель данных для сотрудника
-class Employee(Base):
-    __tablename__ = "employees"
-    id = Column(String, primary_key=True, index=True)
-    role = Column(String)
-    email = Column(String)
-    experience = Column(Integer)
-    deletion_date = Column(String)
 
 # Создание таблицы в базе данных
 Base.metadata.create_all(bind=engine)
-
-# Модель данных для запроса создания/обновления сотрудника
-class EmployeeCreateUpdate(BaseModel):
-    id: str
-    role: str
-    email: str
-    experience: int
-    deletion_date: str
-# Валидация данных
-    @field_validator('id')
-    def validate_passport_id(cls, value):
-        if not re.match(r'^\d{4}-\d{6}$', value):
-            raise ValueError('Invalid passport ID format')
-        return value
-    @field_validator('role')
-    def validate_role(cls, value):
-        if not re.fullmatch('Админ', value) or re.fullmatch('Работник', value):
-            raise ValueError('Invalid role. Choose <Админ> or <Работник>')
-        return value
-    @field_validator('email')
-    def validate_email(cls, value):
-        if not re.match(r'[\w.-]+@[\w-]+\.[\w.]+', value):
-            raise ValueError('Invalid EMAIL format')
-        return value
-
-
-
-# Модель данных для ответа о сотруднике
-class EmployeeResponse(BaseModel):
-    id: str
-    role: str
-    email: str
-    experience: int
-    deletion_date: str
 
 # Логгирование
 @app.middleware("http://127.0.0.1:8000")
@@ -81,17 +29,18 @@ async def log_requests(request, call_next):
 # Получить информацию о сотруднике по идентификатору
 @app.get("/employee/{id}", response_model=EmployeeResponse)
 async def get_employee(id: str):
+    valid_id = EmployeeCreateUpdate.validate_passport_id(id)
     db = SessionLocal()
-    employee = db.query(Employee).filter(Employee.id == id).first()
+    employee = db.query(Employee).filter(Employee.id == valid_id and Employee.is_active == True).first()
     if employee is None:
         raise HTTPException(status_code=404, detail="Employee not found")
     return employee
 
 # Получить информацию о всех сотрудниках с пагинацией
-@app.get("/employee")
+@app.get("/employee", response_model=list[EmployeeResponse])
 async def get_employees(current_page: int = 1, page_size: int = 10):
     db = SessionLocal()
-    employees = db.query(Employee).offset((current_page - 1) * page_size).limit(page_size).all()
+    employees = db.query(Employee).filter(Employee.is_active == True).offset((current_page - 1) * page_size).limit(page_size).all()
     if len(employees) == 0:
         raise HTTPException(status_code=404, detail="There is no employees")
     return employees
@@ -117,7 +66,7 @@ async def create_employee(employee: EmployeeCreateUpdate):
     return new_employee
 
 # Обновить информацию о сотруднике
-@app.put("/employee", response_model=EmployeeResponse)
+@app.patch("/employee", response_model=EmployeeResponse)
 async def update_employee(employee: EmployeeCreateUpdate):
     db = SessionLocal()
     existing_employee = db.query(Employee).filter(Employee.id == employee.id).first()
@@ -142,7 +91,7 @@ async def delete_employee(id: str):
     existing_employee = db.query(Employee).filter(Employee.id == id).first()
     if existing_employee is None:
         raise HTTPException(status_code=404, detail="Employee not found")
-    db.delete(existing_employee)
+    existing_employee.is_active = False
     db.commit()
     return {"message": "Employee deleted successfully"}
 
---
+++ models/EmployeeModels.py
@@ -0,0 +1,56 @@
+from pydantic import BaseModel, field_validator
+from typing import Optional
+from sqlalchemy import Column, Integer, String, Date, Boolean
+from sqlalchemy.orm import DeclarativeBase
+
+# Базовая модель
+class Base(DeclarativeBase):
+    pass
+
+# Модель данных для сотрудника
+class Employee(Base):
+    __tablename__ = "employees"
+    id = Column(String, primary_key=True, index=True)
+    role = Column(String)
+    email = Column(String)
+    experience = Column(Integer)
+    deletion_date = Column(String)
+    is_active = Column(Boolean, default=True)
+
+# Модель данных для запроса создания/обновления сотрудника
+class EmployeeCreateUpdate(BaseModel):
+    id: str
+    role: Optional[str] = None
+    email: Optional[str] = None
+    experience: int
+    deletion_date: str
+# Валидация данных
+    @field_validator('id')
+    def validate_passport_id(cls, value):
+        if not re.match(r'^\d{4}-\d{6}$', value):
+            raise ValueError('Invalid passport ID format')
+        return value
+    @field_validator('role')
+    def validate_role(cls, value):
+        if not value:
+            return value
+        if not re.fullmatch('Админ', value) or re.fullmatch('Работник', value):
+            raise ValueError('Invalid role. Choose <Админ> or <Работник>')
+        return value
+    @field_validator('email')
+    def validate_email(cls, value):
+        if not value:
+            return value
+        if not re.match(r'[\w.-]+@[\w-]+\.[\w.]+', value):
+            raise ValueError('Invalid EMAIL format')
+        return value
+
+
+
+# Модель данных для ответа о сотруднике
+class EmployeeResponse(BaseModel):
+    id: str
+    role: str
+    email: str
+    experience: int
+    deletion_date: str
